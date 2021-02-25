c7590be0f5ff360be7abbaa51809394c1b8975f1
Luis Angel <santosangel748@gmail.com>
2021-02-24 20:36:25 -0600

    Delete con TDD

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -67,4 +67,16 @@ class PostControllerTest extends TestCase{
         
         $this->assertDatabaseHas('posts', ['title' => 'nuevo']);
     }
+
+    public function test_delete()
+    {
+        $post=factory(Post::class)->create();
+
+        $response = $this->json('DELETE', "/api/posts/$post->id");
+
+        $response->assertSee(null)
+                 ->assertStatus((204));
+        
+        $this->assertDatabaseMissing('posts', ['title' => $post->id]);
+    }
 }
